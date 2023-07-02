d98031ed8ebf5a87b8ab938bcd0ddffac99581bb
Crypstick <88693462+Crypstick@users.noreply.github.com>
2023-07-02 18:31:29 +0800

    a few projects created
    experimentation with PID

--- pythonTestingCode1/src/main.py
+++ pythonTestingCode1/src/main.py
@@ -28,6 +28,39 @@ cataMotor_1 = Motor(Ports.PORT8, GearSetting.RATIO_18_1, False)
 
 limit_switch_a = Limit(brain.three_wire_port.a)
 
+#PID Code
+
+#   Please change values according to what needs to be done
+#   Genuinely have no idea what the hell the values are meant
+#   to be, I think I gotta ask Zachary on what I gotta do to
+#   get the values.
+
+#   Usage: PID(error, kp, ki, kd, starti, settle_error, settle_time, timeout)
+#   and somehow, BOOM!???? IDK ANYMORE MAN
+
+class PID:
+    def _init_(self, error, kp, ki, kd, starti, settle_error, settle_time, timeout):
+        self.error = 0
+        self.kp = 0
+        self.ki = 0
+        self.kd = 0
+        self.starti = 0
+        self.settle_error = 0
+        self.settle_time = 0
+        self.timeout = 0
+        self.accumulated_error = 0
+        self.previous_error = 0
+        self.output = 0
+        self.time_spent_settled = 0
+        self.time_spent_running = 0
+
+    def compute(self, error):
+        pass
+
+    def is_settled(self):
+        pass
+  
+
 
 def remoteControl(Remote, rightFrontMotor, rightBackMotor, leftFrontMotor, leftBackMotor):
   while True:
@@ -37,6 +70,7 @@ def remoteControl(Remote, rightFrontMotor, rightBackMotor, leftFrontMotor, leftB
     sideways = Remote.axis1.position()
     leftTrain = fwd + sideways
     rightTrain = fwd - sideways
+
 
     #drivetrain
     rightFrontMotor.set_velocity(rightTrain, PERCENT)
---
+++ vsjklfjdf/src/main.py
@@ -0,0 +1,19 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       kevinorjalo                                                  #
+# 	Created:      30/06/2023, 15:29:11                                         #
+# 	Description:  V5 project                                                   #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain=Brain()
+
+brain.screen.print("Hello V5")
+
+
+        
