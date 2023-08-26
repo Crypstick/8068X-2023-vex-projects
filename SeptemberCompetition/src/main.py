4871aef78568cbc88a1bb93fe0138f4ae0bc8864
Crypstick <88693462+Crypstick@users.noreply.github.com>
2023-08-26 14:27:14 +0800

    september code update

--- SeptemberCompetition/src/main.py
+++ SeptemberCompetition/src/main.py
@@ -1,3 +1,22 @@
+#region VEXcode Generated Robot Configuration
+from vex import *
+
+# wait for rotation sensor to fully initialize
+wait(30, MSEC)
+
+
+def play_vexcode_sound(sound_name):
+    # Helper to make playing sounds from the V5 in VEXcode easier and
+    # keeps the code cleaner by making it clear what is happening.
+    print("VEXPlaySound:" + sound_name)
+    wait(5, MSEC)
+
+# add a small delay to make sure we don't print in the middle of the REPL header
+wait(200, MSEC)
+# clear the console to make sure we don't have the REPL in the console
+print("\033[2J")
+
+#endregion VEXcode Generated Robot Configuration
 # ---------------------------------------------------------------------------- #
 #                                                                              #
 # 	Module:       main.py                                                      #
@@ -15,18 +34,25 @@ brain = Brain()
 Remote = Controller(PRIMARY)
 
 
-
-
 #Motor configurations
-rightFrontMotor = Motor(Ports.PORT18, GearSetting.RATIO_6_1, False)
-rightMiddleMotor = Motor(Ports.PORT19, GearSetting.RATIO_6_1, False)
-rightBackMotor = Motor(Ports.PORT20, GearSetting.RATIO_6_1, True)
+rightFrontMotor = Motor(Ports.PORT1, GearSetting.RATIO_6_1, False)
+rightMiddleMotor = Motor(Ports.PORT2, GearSetting.RATIO_6_1, False)
+rightBackMotor = Motor(Ports.PORT3, GearSetting.RATIO_6_1, True)
 rightDriveSmart = MotorGroup(rightFrontMotor, rightMiddleMotor, rightBackMotor)
-leftFrontMotor = Motor(Ports.PORT8, GearSetting.RATIO_6_1, False)
-leftMiddleMotor = Motor(Ports.PORT7, GearSetting.RATIO_6_1, True)
-leftBackMotor = Motor(Ports.PORT6, GearSetting.RATIO_6_1, False)
+leftFrontMotor = Motor(Ports.PORT20, GearSetting.RATIO_6_1, False)
+leftMiddleMotor = Motor(Ports.PORT19, GearSetting.RATIO_6_1, False)
+leftBackMotor = Motor(Ports.PORT18, GearSetting.RATIO_6_1, True)
 leftDriveSmart = MotorGroup(leftFrontMotor, leftMiddleMotor, leftBackMotor)
 driveTrain = DriveTrain(leftDriveSmart, rightDriveSmart, units=MM)
+
+
+rightFrontMotor.set_stopping(BRAKE)
+rightMiddleMotor.set_stopping(BRAKE)
+rightBackMotor.set_stopping(BRAKE)
+leftFrontMotor.set_stopping(BRAKE)
+leftMiddleMotor.set_stopping(BRAKE)
+leftBackMotor.set_stopping(BRAKE)
+
 
 # wait for rotation sensor to initialise
 wait(30, MSEC)
@@ -35,21 +61,39 @@ wait(30, MSEC)
 rollerMotor_1 = Motor(Ports.PORT10, GearSetting.RATIO_6_1, True)
 cataMotor_1 = Motor(Ports.PORT9, GearSetting.RATIO_18_1, True)
 
-limit_switch_h = Limit(brain.three_wire_port.h)
+limit_switch_h = Limit(brain.three_wire_port.a)
+wall_solonoid = DigitalOut(brain.three_wire_port.h)
+
+wall_solonoid.set(False)
 
 #autonomous code blocks for simplification
-def goStraight(speed, distance, True_or_False):
-  driveTrain.drive_for(FORWARD, distance, MM, velocity=speed, units_v=VelocityUnits.PERCENT, wait=True_or_False)
+def goStraight(speed, distance):
+  leftFrontMotor.spin_for(FORWARD, distance, DEGREES, speed, PERCENT, wait=False)
+  leftMiddleMotor.spin_for(FORWARD, distance, DEGREES, speed, PERCENT, wait=False)
+  leftBackMotor.spin_for(FORWARD, distance, DEGREES, speed, PERCENT, wait=False)
+  rightFrontMotor.spin_for(FORWARD, distance, DEGREES, speed, PERCENT, wait=False)
+  rightMiddleMotor.spin_for(FORWARD, distance, DEGREES, speed, PERCENT, wait=False)
+  rightBackMotor.spin_for(FORWARD, distance, DEGREES, speed, PERCENT, wait=True)
 
-def rotateRight(speed, angle, True_or_False):
-  driveTrain.turn_for(RIGHT, angle, DEGREES, speed, VelocityUnits.PERCENT, wait=True_or_False)
+def rotateRight(speed, distance):
+  leftFrontMotor.spin_for(FORWARD, -1*distance, DEGREES, speed, PERCENT, wait=False)
+  leftMiddleMotor.spin_for(FORWARD, -1*distance, DEGREES, speed, PERCENT, wait=False)
+  leftBackMotor.spin_for(FORWARD, -1*distance, DEGREES, speed, PERCENT, wait=False)
+  rightFrontMotor.spin_for(FORWARD, distance, DEGREES, speed, PERCENT, wait=False)
+  rightMiddleMotor.spin_for(FORWARD, distance, DEGREES, speed, PERCENT, wait=False)
+  rightBackMotor.spin_for(FORWARD, distance, DEGREES, speed, PERCENT, wait=True)
 
-def rotateLeft(speed, angle, True_or_False):
-  driveTrain.turn_for(RIGHT, angle, DEGREES, speed, VelocityUnits.PERCENT, wait=True_or_False)
+def rotateLeft(speed, distance):
+  leftFrontMotor.spin_for(FORWARD, distance, DEGREES, speed, PERCENT, wait=False)
+  leftMiddleMotor.spin_for(FORWARD, distance, DEGREES, speed, PERCENT, wait=False)
+  leftBackMotor.spin_for(FORWARD, distance, DEGREES, speed, PERCENT, wait=False)
+  rightFrontMotor.spin_for(FORWARD, -1*distance, DEGREES, speed, PERCENT, wait=False)
+  rightMiddleMotor.spin_for(FORWARD, -1*distance, DEGREES, speed, PERCENT, wait=False)
+  rightBackMotor.spin_for(FORWARD, -1*distance, DEGREES, speed, PERCENT, wait=True)
 
 
 def autonomous():
-  rollerMotor_1.spin(FORWARD)
+  rotateLeft(80, 500)
   # #moving forward to push alliance triball inside, TO CHANGE VALUES
   # rightFrontMotor.spin_for(FORWARD, -600, DEGREES, wait=False)
   # rightMiddleMotor.spin_for(FORWARD, -600, DEGREES, wait=False)
@@ -103,63 +147,83 @@ def autonomous():
 
 
 
-def driver_control(Remote, rightFrontMotor, rightMiddleMotor, rightBackMotor, leftFrontMotor, leftMiddleMotor, leftBackMotor):
+def driver_drivetrain():
   while True:
     #init
-    fwd = Remote.axis1.position()*-1
-    sideways = Remote.axis3.position()
+    fwd = Remote.axis1.position() * -1
+    sideways = Remote.axis3.position() 
     rightTrain = fwd - sideways
     leftTrain = fwd + sideways
 
 
     #drivetrain
-    rightFrontMotor.set_velocity(rightTrain, PERCENT)
-    rightMiddleMotor.set_velocity(rightTrain, PERCENT)
-    rightBackMotor.set_velocity(rightTrain, PERCENT)
-    leftFrontMotor.set_velocity(leftTrain, PERCENT)
-    leftMiddleMotor.set_velocity(leftTrain, PERCENT)
-    leftBackMotor.set_velocity(leftTrain, PERCENT)
-    rightFrontMotor.spin(FORWARD)
-    rightMiddleMotor.spin(FORWARD)
-    rightBackMotor.spin(FORWARD)
-    leftFrontMotor.spin(FORWARD)
-    leftMiddleMotor.spin(FORWARD)
-    leftBackMotor.spin(FORWARD)
+    rightDriveSmart.set_velocity(rightTrain, PERCENT)
+    leftDriveSmart.set_velocity(leftTrain, PERCENT)
 
-    #roller intake control
-    if Remote.buttonL1.pressing():
-      rollerMotor_1.set_velocity(100, PERCENT)
-      rollerMotor_1.spin(FORWARD)
-    elif Remote.buttonL2.pressing():
-      rollerMotor_1.set_velocity(-100, PERCENT)
-      rollerMotor_1.spin(FORWARD)
-    else:
-      rollerMotor_1.stop()
+    # rightFrontMotor.set_velocity(rightTrain, PERCENT)
+    # rightMiddleMotor.set_velocity(rightTrain, PERCENT)
+    # rightBackMotor.set_velocity(rightTrain, PERCENT)
+    # leftFrontMotor.set_velocity(leftTrain, PERCENT)
+    # leftMiddleMotor.set_velocity(leftTrain, PERCENT)
+    # leftBackMotor.set_velocity(leftTrain, PERCENT)
 
-    #cata control + limit switch code
-    '''
-    if Remote.buttonY.pressing():
-      while 1 == 1:
-        cataMotor_1.stop()
-        cataMotor_1.set_velocity(0,PERCENT)
-    if limit_switch_h.pressing() == 0:
-      cataMotor_1.set_velocity(100, PERCENT)
-      cataMotor_1.spin(FORWARD)
-      print("NOT PRESSED", limit_switch_h.pressing())
-    elif limit_switch_h.pressing() == 1:
-      cataMotor_1.stop()
-      cataMotor_1.set_velocity(0, PERCENT)
-      cataMotor_1.spin(FORWARD)
-      cataMotor_1.stop()
-      print("PRESSED", limit_switch_h.pressing())
-      '''
-    if Remote.buttonR1.pressing() == True:
-      while Remote.buttonR1.pressing() == True:
-        cataMotor_1.set_velocity(100, PERCENT)
-        cataMotor_1.spin(FORWARD)
-    else:
-      cataMotor_1.set_velocity(0, PERCENT)
-      cataMotor_1.stop()
+
+    rightDriveSmart.spin(FORWARD)
+    leftDriveSmart.spin(FORWARD)
+
     
 
-competition = Competition(autonomous, driver_control(Remote, rightFrontMotor, rightMiddleMotor, rightBackMotor, leftFrontMotor, leftMiddleMotor, leftBackMotor))
+
+def driver_rollerIntake():
+  if Remote.buttonL1.pressing():
+    rollerMotor_1.set_velocity(100, PERCENT)
+    rollerMotor_1.spin(FORWARD)
+  elif Remote.buttonR1.pressing():
+    rollerMotor_1.set_velocity(-100, PERCENT)
+    rollerMotor_1.spin(FORWARD)
+  else:
+    rollerMotor_1.stop()
+
+def driver_pneumatics():
+  if Remote.buttonY.pressing():
+      wall_solonoid.set(True)
+  else:
+      wall_solonoid.set(False)
+
+
+    #cata control + limit switch code
+'''
+if Remote.buttonY.pressing():
+  while 1 == 1:
+    cataMotor_1.stop()
+    cataMotor_1.set_velocity(0,PERCENT)
+if limit_switch_h.pressing() == 0:
+  cataMotor_1.set_velocity(100, PERCENT)
+  cataMotor_1.spin(FORWARD)
+  print("NOT PRESSED", limit_switch_h.pressing())
+elif limit_switch_h.pressing() == 1:
+  cataMotor_1.stop()
+  cataMotor_1.set_velocity(0, PERCENT)
+  cataMotor_1.spin(FORWARD)
+  cataMotor_1.stop()
+  print("PRESSED", limit_switch_h.pressing())
+  '''
+def driver_catacontrol():
+  if Remote.buttonR2.pressing() == True:
+    while Remote.buttonR2.pressing() == True:
+      cataMotor_1.set_velocity(100, PERCENT)
+      cataMotor_1.spin(FORWARD)
+  else:
+    cataMotor_1.set_velocity(0, PERCENT)
+    cataMotor_1.stop()
+
+def driver_control():
+
+    while 1:
+        driver_drivetrain()
+        driver_rollerIntake()
+        driver_catacontrol()
+        driver_pneumatics()
+
+'driver_control(Remote, rightFrontMotor, rightMiddleMotor, rightBackMotor, leftFrontMotor, leftMiddleMotor, leftBackMotor)'
+competition = Competition(driver_control(), autonomous)
